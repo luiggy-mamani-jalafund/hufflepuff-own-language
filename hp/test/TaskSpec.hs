6663464a36d4d96a1b85bd09de75f5848c4232f7
DiegoFigueroaS <diegofigueroa_dev@outlook.com>
2024-06-28 17:45:12 -0400

    test cases completed

--- hp/test/TaskSpec.hs
+++ hp/test/TaskSpec.hs
@@ -1,57 +1,97 @@
+-- File: test/TaskSpec.hs
 {-# LANGUAGE OverloadedStrings #-}
 
 module TaskSpec (spec) where
 
 import Test.Hspec
-import AbstractSyntaxTree
-import HpParser (task')
-import SymbolTable (emptyTable)
 import Text.Parsec (parse)
+import HpParser
+import AbstractSyntaxTree
+import SymbolTable (emptyTable)
+import Lexer (whiteSpace)
 
 spec :: Spec
 spec = do
   describe "Task Parser" $ do
-    it "parses a simple task" $ do
-      let input = "Task { title: \"My Task\", description: \"This is a test task\", state: \"open\", members: ListMember [], tag: noTag, subTasks: ListTask [] }"
-      let expectedTask = Task
-            { title = TaskValueTitle (String "My Task")
-            , description = TaskValueDescription (String "This is a test task")
-            , state = TaskValueState (StringId "open")
+
+    it "parses Task with TaskValueTitle" $ do
+      let input = "Task { title: \"Title\", description: \"Description\", state: \"State\", members: List:Member[], tag: NoTag, subTasks: List:Task[] }"
+          result = parse (whiteSpace *> task' emptyTable) "" input
+          expectedTask = Task 
+            { title = TaskValueTitle (String "Title")
+            , description = TaskValueDescription (String "Description")
+            , state = TaskValueState (StringId "State")
             , members = TaskValueMembers (ListMember [])
             , tag = TaskValueTag NoTag
             , subTasks = TaskValueSubTasks (ListTask [])
             }
-      let result = parse (task' emptyTable) "" input
       case result of
         Right (task, _) -> task `shouldBe` expectedTask
-        Left err -> expectationFailure (show err)
+        _               -> expectationFailure "Parsing failed"
 
-    it "parses a task with identifier titles" $ do
-      let input = "Task { title: titleId, description: \"Task with identifier title\", state: \"inProgress\", members: ListMember [], tag: noTag, subTasks: ListTask [] }"
-      let expectedTask = Task
+    it "parses Task with TaskIdentifierTitle" $ do
+      let input = "Task { title: titleId, description: descriptionId, state: stateId, members: membersId, tag: tagId, subTasks: subTasksId }"
+          result = parse (whiteSpace *> task' emptyTable) "" input
+          expectedTask = Task 
             { title = TaskIdentifierTitle "titleId"
-            , description = TaskValueDescription (String "Task with identifier title")
-            , state = TaskValueState (StringId "inProgress")
+            , description = TaskIdentifierDescription "descriptionId"
+            , state = TaskIdentifierState "stateId"
+            , members = TaskIdentifierMembers "membersId"
+            , tag = TaskIdentifierTag "tagId"
+            , subTasks = TaskIdentifierSubTasks "subTasksId"
+            }
+      case result of
+        Right (task, _) -> task `shouldBe` expectedTask
+        _               -> expectationFailure "Parsing failed"
+
+    it "parses Task with TaskTakeTitle" $ do
+      let input = "Task { title: takeTitle.title, description: takeDescription.description, state: takeState.state, members: takeMembers.members, tag: takeTag.tag, subTasks: takeSubTasks.subTasks }"
+          result = parse (whiteSpace *> task' emptyTable) "" input
+          expectedTask = Task 
+            { title = TaskTakeTitle "takeTitle"
+            , description = TaskTakeDescription "takeDescription"
+            , state = TaskTakeState "takeState"
+            , members = TaskTakeMembers "takeMembers"
+            , tag = TaskTakeTag "takeTag"
+            , subTasks = TaskTakeSubTasks "takeSubTasks"
+            }
+      case result of
+        Right (task, _) -> task `shouldBe` expectedTask
+        _               -> expectationFailure "Parsing failed"
+
+    it "parses Task with mixed attributes" $ do
+      let input = "Task { title: \"Mixed Title\", description: descriptionId, state: takeState.state, members: List:Member[], tag: NoTag, subTasks: List:Task[] }"
+          result = parse (whiteSpace *> task' emptyTable) "" input
+          expectedTask = Task 
+            { title = TaskValueTitle (String "Mixed Title")
+            , description = TaskIdentifierDescription "descriptionId"
+            , state = TaskTakeState "takeState"
             , members = TaskValueMembers (ListMember [])
             , tag = TaskValueTag NoTag
             , subTasks = TaskValueSubTasks (ListTask [])
             }
-      let result = parse (task' emptyTable) "" input
       case result of
         Right (task, _) -> task `shouldBe` expectedTask
-        Left err -> expectationFailure (show err)
+        _               -> expectationFailure "Parsing failed"
 
-    it "parses a task with take attributes" $ do
-      let input = "Task { title: takeTitleId, description: takeDescriptionId, state: takeStateId, members: takeMembersId, tag: takeTagId, subTasks: takeSubTasksId }"
-      let expectedTask = Task
-            { title = TaskIdentifierTitle "takeTitleId"
-            , description = TaskIdentifierDescription "takeDescriptionId"
-            , state = TaskIdentifierState "takeStateId"
-            , members = TaskIdentifierMembers "takeMembersId"
-            , tag = TaskIdentifierTag "takeTagId"
-            , subTasks = TaskIdentifierSubTasks "takeSubTasksId"
+    it "parses Task with empty lists" $ do
+      let input = "Task { title: titleId, description: descriptionId, state: stateId, members: List:Member[], tag: NoTag, subTasks: List:Task[] }"
+          result = parse (whiteSpace *> task' emptyTable) "" input
+          expectedTask = Task 
+            { title = TaskIdentifierTitle "titleId"
+            , description = TaskIdentifierDescription "descriptionId"
+            , state = TaskIdentifierState "stateId"
+            , members = TaskValueMembers (ListMember [])
+            , tag = TaskValueTag NoTag
+            , subTasks = TaskValueSubTasks (ListTask [])
             }
-      let result = parse (task' emptyTable) "" input
       case result of
         Right (task, _) -> task `shouldBe` expectedTask
-        Left err -> expectationFailure (show err)
+        _               -> expectationFailure "Parsing failed"
+
+    it "fails to parse Task with missing attributes" $ do
+      let input = "Task { title: titleId, description: descriptionId, state: stateId }"
+          result = parse (whiteSpace *> task' emptyTable) "" input
+      case result of
+        Left _  -> return () -- Expected to fail
+        Right _ -> expectationFailure "Parsing should have failed"
